6cc6230f8edf1d4532a5cdcb05e72ec23721c6ee
Michael Permann <mike.permann@gmail.com>
2022-06-06 09:41:40 -0500

    Update AppUpdaterDeferralForced.bash
    Minor modifications to add script name.

--- AppUpdaterDeferralForced.bash
+++ AppUpdaterDeferralForced.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Name: AppUpdaterDeferralForced.bash
+# Version: 1.0.4
+# Created: 05-17-2022 by Michael Permann
+# Updated: 06-04-2022
 # The script is for patching an app with user notification before starting, if the app is running. It supports
 # deferrals with tracking and forced install after deferrals run out. If the app is not running, it will be 
 # silently patched without any notification to the user. Parameter 4 is the name of the app to patch. Parameter
 # 5 is the name of the app process. Parameter 6 is the policy trigger name for the policy installing the app.
 # Parameter 7 is the number of allowed deferrals. Parameter 8 is the countdown timer in seconds. The script is
 # relatively basic and can't currently kill more than one process or patch more than one app.
-# Version 1.0.4
-# Created 05-17-2022 by Michael Permann
-# Updated 06-04-2022
 
 APP_NAME=$4
 APP_PROCESS_NAME=$5
